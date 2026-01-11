3983a0074a95b9959b80f8edfa7148502fe4e040
thinhdaniel867 <b9m67wzrbt@privaterelay.appleid.com>
2026-01-12 04:12:38 +0700

    puzzle.py

---
+++ knights/puzzle.py
@@ -0,0 +1,62 @@
+from logic import *
+
+AKnight = Symbol("A is a Knight")
+AKnave = Symbol("A is a Knave")
+
+BKnight = Symbol("B is a Knight")
+BKnave = Symbol("B is a Knave")
+
+CKnight = Symbol("C is a Knight")
+CKnave = Symbol("C is a Knave")
+
+# Puzzle 0
+knowledge0 = And(
+    Or(AKnight, AKnave), Not(And(AKnight, AKnave)),
+    Biconditional(AKnight, And(AKnight, AKnave))
+)
+
+# Puzzle 1
+knowledge1 = And(
+    Or(AKnight, AKnave), Not(And(AKnight, AKnave)),
+    Or(BKnight, BKnave), Not(And(BKnight, BKnave)),
+    Biconditional(AKnight, And(AKnave, BKnave))
+)
+
+# Puzzle 2
+knowledge2 = And(
+    Or(AKnight, AKnave), Not(And(AKnight, AKnave)),
+    Or(BKnight, BKnave), Not(And(BKnight, BKnave)),
+    Biconditional(AKnight, Biconditional(AKnight, BKnight)),
+    Biconditional(BKnight, Biconditional(AKnight, BKnave))
+)
+
+# Puzzle 3
+knowledge3 = And(
+    Or(AKnight, AKnave), Not(And(AKnight, AKnave)),
+    Or(BKnight, BKnave), Not(And(BKnight, BKnave)),
+    Or(CKnight, CKnave), Not(And(CKnight, CKnave)),
+    Biconditional(AKnight, Or(AKnight, AKnave)),
+    Biconditional(BKnight, Biconditional(AKnight, AKnave)),
+    Biconditional(BKnight, CKnave),
+    Biconditional(CKnight, AKnight)
+)
+
+def main():
+    symbols = [AKnight, AKnave, BKnight, BKnave, CKnight, CKnave]
+    puzzles = [
+        ("Puzzle 0", knowledge0),
+        ("Puzzle 1", knowledge1),
+        ("Puzzle 2", knowledge2),
+        ("Puzzle 3", knowledge3)
+    ]
+    for puzzle, knowledge in puzzles:
+        print(puzzle)
+        if len(knowledge.conjuncts) == 0:
+            print("    Not yet implemented.")
+        else:
+            for symbol in symbols:
+                if model_check(knowledge, symbol):
+                    print(f"    {symbol}")
+
+if __name__ == "__main__":
+    main()
